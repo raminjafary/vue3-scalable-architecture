6e3353df59b9447990c94a9ebb9ba46f628ea0f5
raminjafary <raminjafary1993@gmail.com>
2023-02-23 11:06:03 +0330

    feat: multi env build via cli args

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
 import { defineConfig } from 'vite'
 import eslintPlugin from 'vite-plugin-eslint'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import fs from 'fs'
+import * as dotenv from 'dotenv'
+
+const env = process.env.NODE_ENV
+
+const envPath = path.resolve(process.cwd(), `.env.${env}`)
+const defaultEnvPath = path.resolve(process.cwd(), '.env.development')
+
+dotenv.config({
+	path: fs.existsSync(envPath) ? envPath : defaultEnvPath,
+})
 
 export default defineConfig({
 	plugins: [vue(), eslintPlugin()],
 	server: {
 		host: true,
 	},
+	mode: env,
 	resolve: {
 		alias: [{ find: '@', replacement: '/src' }],
 	},

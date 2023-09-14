4d6df9a998616b5863f50327a4ed2c55410c91f4
raminjafary <raminjafary1993@gmail.com>
2023-09-14 14:47:35 +0330

    chore: bump dependencies version

--- scripts/create-merge-request.js
+++ scripts/create-merge-request.js
@@ -7,8 +7,7 @@ exec(
 	'git push origin HEAD \
         -o merge_request.create \
         -o merge_request.remove_source_branch \
-        -o merge_request.target=' +
-		targetBranch,
+        -o merge_request.target=' + targetBranch,
 	(error, stdout, stderr) => {
 		stdout && console.log(`[stdout]\n${stdout}`)
 		stderr && console.log(`[stderr]\n${stderr}`)
